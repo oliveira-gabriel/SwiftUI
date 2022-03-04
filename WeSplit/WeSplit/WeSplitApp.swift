2965c9d528dc70afb607102b7cc598237e965532
Gabriel Oliveira <gabrieloliveira472@gmail.com>
2022-03-03 21:47:20 -0300

    test

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Gabriel Oliveira on 03/03/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

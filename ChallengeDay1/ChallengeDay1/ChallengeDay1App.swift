a057c356a84f32f2d73d9395b3ef3e289f8e97b4
Gabriel Oliveira <gabrieloliveira472@gmail.com>
2022-03-05 12:15:54 -0300

    feat: add Challenger app init

---
+++ ChallengeDay1/ChallengeDay1/ChallengeDay1App.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeDay1App.swift
+//  ChallengeDay1
+//
+//  Created by Gabriel Oliveira on 05/03/22.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeDay1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

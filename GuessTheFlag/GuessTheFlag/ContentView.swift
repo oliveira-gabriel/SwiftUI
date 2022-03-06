93f6177a4713d9f9f464c44daf8da8bc54032e08
Gabriel Oliveira <gabrieloliveira472@gmail.com>
2022-03-06 14:47:02 -0300

    feat: buttons and images

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -2,8 +2,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center)
+//        VStack {
+//            Button("Button 1") {}
+//                .buttonStyle(.bordered)
+//            Button("Button 2", role: .destructive) {}
+//                .buttonStyle(.bordered)
+//            Button("Button 3") {}
+//                .buttonStyle(.borderedProminent)
+//                .tint(.mint)
+//            Button("Button 4", role: .destructive) {}
+//                .buttonStyle(.borderedProminent)
+//        }
 
+        Button {
+            print("Button was tapped")
+        } label: {
+            Label("Image", systemImage: "pencil")
+        }
+
+
+    }
+
+    func executeDelete() {
+        print("Now deleting")
     }
 }
 

f55b090416a7aa6a34e391ca2d72d96e27357418
Gabriel Oliveira <gabrieloliveira472@gmail.com>
2022-03-06 13:23:27 -0300

    feat: init GuessTheFlag

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Gabriel Oliveira on 06/03/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

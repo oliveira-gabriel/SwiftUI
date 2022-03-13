69e8c5cfa2d99fbd841d77e6c9a4673bab2be718
Gabriel Oliveira <gabrieloliveira472@gmail.com>
2022-03-13 04:43:52 -0300

    feat: update styles, add animation, and acessebility

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -1,89 +1,128 @@
 import SwiftUI
 
+struct FlagImage: View {
+    var image: String
+
+    var body: some View {
+//        content
+        Image(image)
+        .renderingMode(.original)
+        .clipShape(Capsule())
+        .overlay(Capsule().stroke(Color.white, lineWidth: 5))
+        .shadow(color: .gray, radius: 25)
+    }
+}
+
 struct ContentView: View {
 
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+    ]
+
+    @State private var correctAnswer = Int.random(in: 0...2)
 
     @State private var showingScore = false
     @State private var scoreTitle = ""
-    @State private var countries = ["Estonia", "France", "Germany", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
-    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var scoreMessage = ""
     @State private var score = 0
 
-    func flagTapped(_ number: Int) {
-        if number == correctAnswer {
-            scoreTitle = "Correct"
-            score += 1
-        }else {
-            scoreTitle = "Wrong, that's the flag of \(countries[number])"
-            score -= 1
-        }
+    @State private var opacityAmount = 1.0
+    @State private var rotationAmount = 0.0
+    @State private var wrongRotationAmount = [0.0, 0.0, 0.0]
 
+    var body: some View {
+
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .purple]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
+
+            VStack(spacing: 30) {
+                Spacer()
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .autocapitalization(.allCharacters)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                }
+
+                ForEach(0 ..< 3, id: \.self) { number in
+
+                    Button(action: {
+                        opacityAmount = 0.8
+                        answerTapped(number)
+                    }) {
+                        FlagImage(image: countries[number])
+                            .accessibility(label: Text(labels[countries[number], default: "Unknown Flag"]))
+                    }
+                    .opacity(number == correctAnswer ? 1 : opacityAmount)
+                    .rotation3DEffect(
+                        .degrees(number == correctAnswer ? rotationAmount : 0),
+                                      axis: (x: 0, y: 1, z: 0))
+                    .rotation3DEffect(.degrees(wrongRotationAmount[number]),
+                                      axis: (x: 0, y: 1, z: 0))
+                }
+
+                Text("SCORE: \(score)")
+                    .foregroundColor(.white)
+                    .font(.headline)
+                Spacer()
+            }
+        }
+        .alert(isPresented: $showingScore) {
+            Alert(title: Text(scoreTitle), message: Text(scoreMessage), dismissButton: .default(Text("Continue")) {
+                askQuestion()
+                })
+        }
+    }
+
+    func answerTapped(_ number: Int) {
+        if number == correctAnswer {
+            score += 1
+            scoreTitle = "CORRECT!"
+            scoreMessage = "Your score is: \(score)"
+            rotationAmount = 0.0
+
+            withAnimation(.interpolatingSpring(stiffness: 20, damping: 5)) {
+                rotationAmount = 360
+            }
+
+        } else {
+            score -= 1
+            scoreTitle = "!WRONG!"
+            scoreMessage = "That's the flag of \(countries[number])\nYour score is now: \(score)"
+
+            withAnimation(Animation.interpolatingSpring(mass: 1, stiffness: 120, damping: 40, initialVelocity: 200)) {
+                wrongRotationAmount[number] = 5
+            }
+
+        }
         showingScore = true
     }
 
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
-    }
-    var body: some View {
-        ZStack{
-            RadialGradient(stops: [
-                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
-                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
-            ], center: .top, startRadius: 200, endRadius: 500)
-                .ignoresSafeArea()
-            VStack {
-                Spacer()
-                Text("Guess the Flag")
-                    .font(.largeTitle.bold())
-                    .foregroundColor(.white)
-
-                VStack(spacing: 15){
-                    VStack {
-                        Text("Tap the flag of")
-                            .foregroundStyle(.secondary)
-                            .font(.subheadline.weight(.heavy))
-                        Text(countries[correctAnswer])
-                            .font(.largeTitle.weight(.semibold))
-
-                    }
-                    ForEach(0..<3) { number in
-                        Button {
-                            flagTapped(number)
-                        } label: {
-                            Image(countries[number])
-                                .renderingMode(.original)
-                                .clipShape(Capsule())
-                                .shadow(radius: 5)
-                        }
-                    }
-                }
-                .frame(maxWidth: .infinity)
-                .padding(.vertical, 20)
-                .background(.thinMaterial)
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-
-                Spacer()
-                Spacer()
-
-                Text("Score: \(score)")
-                    .foregroundColor(.white)
-                    .font(.title.bold())
-
-                Spacer()
-            }
+        withAnimation(.easeInOut) {
+            opacityAmount = 1.0
         }
-        .alert(isPresented: $showingScore){
-            Alert(
-                title: Text(scoreTitle),
-                message: Text("Your score is \(score)"),
-                dismissButton: .default(Text("Continue")) {
-                    askQuestion()
-                }
-            )
-        }
-    }
+        rotationAmount = 0.0
+        wrongRotationAmount = Array(repeating: 0.0, count: 3)
 
+    }
 
 }
 

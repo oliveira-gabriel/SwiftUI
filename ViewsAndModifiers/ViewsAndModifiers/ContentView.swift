700ff3fbca0ec0ce344db82348192316b74c756c
Gabriel Oliveira <gabrieloliveira472@gmail.com>
2022-03-14 20:22:20 -0300

    feat: make custom views

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -1,19 +1,25 @@
 import SwiftUI
 
-struct ContentView: View {
-    var motto1: some View {
-        Text("Draco dormiens")
-    }
-    let motto2 = Text("nunquam titillandus")
-
-    @ViewBuilder var spells: some View {
-        Text("Lumos")
-        Text("Obliviate")
-    }
+struct CapsuleText: View {
+    var text: String
 
     var body: some View {
-        VStack {
-            spells
+        Text(text)
+            .font(.largeTitle)
+            .padding()
+            .foregroundColor(.white)
+            .background(.blue)
+            .clipShape(Capsule())
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        VStack(spacing: 10) {
+            CapsuleText(text: "First")
+                    .foregroundColor(.white)
+                CapsuleText(text: "Second")
+                    .foregroundColor(.red)
         }
     }
 }

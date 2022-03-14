e6b5c311942f4a9fce9d2b7d010a3c30ffec4bc5
Gabriel Oliveira <gabrieloliveira472@gmail.com>
2022-03-14 20:23:41 -0300

    feat: add ViewModifier

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -1,25 +1,21 @@
 import SwiftUI
 
-struct CapsuleText: View {
-    var text: String
-
-    var body: some View {
-        Text(text)
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
             .font(.largeTitle)
-            .padding()
             .foregroundColor(.white)
-            .background(.blue)
-            .clipShape(Capsule())
+            .padding()
+            .background(.gray)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
     }
 }
 
 struct ContentView: View {
     var body: some View {
         VStack(spacing: 10) {
-            CapsuleText(text: "First")
-                    .foregroundColor(.white)
-                CapsuleText(text: "Second")
-                    .foregroundColor(.red)
+            Text("Hello World")
+                .modifier(Title())
         }
     }
 }

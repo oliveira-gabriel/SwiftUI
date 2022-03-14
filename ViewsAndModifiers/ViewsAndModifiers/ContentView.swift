3a86c99961b2c44c013659164761ea359b8775d1
Gabriel Oliveira <gabrieloliveira472@gmail.com>
2022-03-14 20:26:34 -0300

    feat: add watermarked  modifiers

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -10,9 +10,29 @@ struct Title: ViewModifier {
             .clipShape(RoundedRectangle(cornerRadius: 10))
     }
 }
+
+struct Watermark: ViewModifier {
+    var text: String
+
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(.black)
+        }
+    }
+}
+
+
 extension View {
     func titleStyle() -> some View {
         modifier(Title())
+    }
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
     }
 }
 
@@ -21,6 +41,10 @@ struct ContentView: View {
         VStack(spacing: 10) {
             Text("Hello World")
                 .titleStyle()
+
+            Color.blue
+                .frame(width: 300, height: 200)
+                .watermarked(with: "Hacking with Swift")
         }
     }
 }
